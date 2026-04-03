07dfc4b424d52e0544e6185fc340b8942ad25c26
suvajit poria <suvajitporia67@gmail.com>
2026-04-04 00:24:08 +0530

    Create 29-DivideTwoIntegers.cs

---
+++ 29-DivideTwoIntegers.cs
@@ -0,0 +1,55 @@
+
+public class Solution {
+    public int Divide(int dividend, int divisor) {
+        int i = 0;
+
+        if (dividend == 0)
+            return 0;
+
+        if ((dividend == int.MinValue || dividend == int.MaxValue)  && divisor == -1)
+        {
+            if(dividend == int.MinValue)
+            {
+                return int.MaxValue;
+            }
+            else
+            {
+                return int.MinValue +1;
+            }
+        }
+        if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
+        {
+            if (dividend > 0)
+            { 
+                dividend = -dividend;
+            }
+            if (divisor > 0)
+            { 
+                divisor = -divisor;
+            }
+            while (dividend <= divisor)
+            {
+                dividend -= divisor;
+                i++;
+            }
+        }
+        else
+        {
+            if (dividend > 0)
+            { 
+                dividend = -dividend;
+            }
+            if (divisor > 0)
+            { 
+                divisor = -divisor;
+            }
+            while (dividend <= divisor)
+            {
+                dividend -= divisor;
+                i--;
+            }
+        }
+
+        return i;
+    }
+}
