a2fe89c8f9247a8b3af2d50a7441a799232aba32
suvajit poria <suvajitporia67@gmail.com>
2026-05-03 19:39:58 +0530

    Create 033-SearchInRotatedSortedArray.cs

---
+++ 033-SearchInRotatedSortedArray.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.Length-1;
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[start] <= nums[mid])
+            {
+                if(target >= nums[start] &&  target <= nums[mid] )
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                if(target > nums[mid] && target <= nums[end])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
